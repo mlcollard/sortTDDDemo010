c37c025548922f4858228872ca98d0df3f04772b
Michael L. Collard <collard@uakron.edu>
2021-10-20 14:42:51 -0400

    Add sort of any two-element vectors

--- sort.cpp
+++ sort.cpp
@@ -6,13 +6,26 @@
 
 #include "sort.hpp"
 
+namespace {
+
+    void swap(int& n1, int& n2) {
+        int t = n1;
+        n1 = n2;
+        n2 = t;
+    }
+
+    void order(int& n1, int& n2) {
+        if (n1 > n2)
+            swap(n1, n2);
+    }
+}
+
 // sort the entire vector in ascending order
 void sort(std::vector<int>& v) {
 
     if (v.size() <= 1)
         return;
 
-    int t = v[0];
-    v[0] = v[1];
-    v[1] = t;
+    order(v[0], v[1]);
 }
+
